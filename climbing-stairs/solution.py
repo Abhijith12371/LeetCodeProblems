9a778a2316eed0a981457e0174bafb1f325b14ad
N V S S Abhijith <162548857+Abhijith12371@users.noreply.github.com>
2026-01-19 16:17:52 +0530

    solution for climbing-stairs

--- climbing-stairs/solution.py
+++ climbing-stairs/solution.py
@@ -6,5 +6,14 @@ class Solution:
             if n not in memo:
                 memo[n]=(countStairs(n-1,memo))+(countStairs(n-2,memo))
             return memo[n]
+        def countStairs_tab(n,ways):
+            for i in range(1,len(ways)):
+                if i==1:
+                    ways[i]=ways[i-1]
+                else:
+                    ways[i]=ways[i-1]+ways[i-2]
+            return ways[n]
         memo={}
-        return countStairs(n,memo)
+        ways=[0]*(n+1)
+        ways[0]=1
+        return countStairs_tab(n,ways)
